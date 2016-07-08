b9958ea6e832b7fdd61619371bc4602aa5a97003
bert <bpoller@ekito.fr>
2016-07-08 17:04:23 +0200

    Some benchmarking stuff.

--- src/main/java/com/monkeypatch/mktd/feignvsretrofit/exo3/ApiFactory.java
+++ src/main/java/com/monkeypatch/mktd/feignvsretrofit/exo3/ApiFactory.java
@@ -1,12 +1,25 @@
 package com.monkeypatch.mktd.feignvsretrofit.exo3;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.monkeypatch.mktd.feignvsretrofit.exo1.MonkeyApi;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.okhttp.OkHttpClient;
 
 class ApiFactory {
 
     static MonkeyApi buildMonkeyApi(String url) {
-        // TODO you should implements this method
-        throw new RuntimeException("Not yet implemented");
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new AfterburnerModule());
+
+        return Feign
+                .builder()
+                .decoder(new JacksonDecoder(mapper))
+                .client(new OkHttpClient())
+                .target(MonkeyApi.class, url + "/monkeys");
     }
 
 }
